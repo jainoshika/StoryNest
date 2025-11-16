d4f3050a54f3c0f42b8919debb0dc20fd0b5e211
Oshika Jain <153307546+jainoshika@users.noreply.github.com>
2025-11-16 22:29:43 +0530

    route correction

--- routes/blog.js
+++ routes/blog.js
@@ -5,17 +5,16 @@ const {handleCreateNewBlog, handleAllBlogs, handleDeleteBlog, handleEditBlog, ha
 
 router
     .get("/", handleAllBlogs)
-    .get("/new", restrictTo(['NORMAL']), (req,res) => { // opens new blog page
-        res.render("newblog");
-    })
+    .get("/new", restrictTo(['NORMAL']), (req,res) => { res.render("newblog"); })
     .get("/search", handleSearchBlog)
+    .get("/edit/:id", handleEditBlog)
+    .get("/:id", handleViewBlog)
+
     .post("/new", restrictTo(['NORMAL']),handleCreateNewBlog)
     .post("/like/:id",restrictTo(['NORMAL']),  handleBlogLike)
     .post("/comment/:id", restrictTo(['NORMAL']), handleBlogComments)
-    .get("/edit/:id", handleEditBlog)
     .post("/edit/:id", restrictTo(['NORMAL']), handleUpdateBlog)
     .post("/delete/:id", restrictTo(['NORMAL']), handleDeleteBlog)
-    .get("/:id", handleViewBlog)
     
 
 
